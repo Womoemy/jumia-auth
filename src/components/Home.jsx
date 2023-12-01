e3676815940a72d087baf2dbf55be39e0674f1cf
Womoemy <womoemiebiobowei@gmail.com>
2023-12-01 11:24:59 +0100

    update home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
     return (
         <>
             <div className="flex flex-col justify-center items-center h-screen">
-                <p>Login Successful!</p>
+                <p>Successful!</p>
                 <p>Happy Shopping!</p>
             </div>
         </>
